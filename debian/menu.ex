71deb466db38165320d9fbe036ea677827cacb83
Andrew Garner <muzazzi@gmail.com>
2013-10-01 19:41:12 +0000

    remove debian/ example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mysql-sandbox):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mysql-sandbox" command="/usr/bin/mysql-sandbox"
--- debian/mysql-sandbox.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mysql-sandbox package
-#
-0 4	* * *	root	[ -x /usr/bin/mysql-sandbox_maintenance ] && /usr/bin/mysql-sandbox_maintenance
